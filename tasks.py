1d41ef5c0d712f27bbeec1495a82789f608e7ece
charushilag <151528035+charushilag@users.noreply.github.com>
2024-08-28 23:46:37 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,3 @@
-# tasks.py
-
 from celery import Celery
 from datetime import datetime
 import uuid
@@ -8,35 +6,38 @@ import psycopg2
 from  testapiconn import execute_task
 from celery.schedules import crontab
 
-# Create a Celery instance
-app = Celery('tasks', broker='redis://localhost:6379/0', backend='rpc://')
 
-app.conf.update(
-    result_backend='db+postgresql://postgres:root@localhost/postgres'
-)
+
+print('in celery instance')
+app = Celery('tasks', broker='redis://localhost:6379/0', backend='db+postgresql://postgres:root@localhost/postgres')
+
+
+print('in beat schedule')
 app.conf.beat_schedule = {
     'run-scheduler-daily': {
         'task': 'tasks.schedule_task',
-        'schedule': 60 ,   #crontab(minute=0, hour=0),
+        'schedule': crontab(minute=0, hour=0) # 86400
     }
-    #,
+    ,
     
-    # 'run-executor-every-5-minutes': {
-    #     'task': 'tasks.run_executor',
-    #     'schedule': 300,
-    # }
+    'run-executor-every-5-minutes': {
+        'task': 'tasks.run_executor',
+        'schedule': crontab(minute=5, hour=0) # 300
+    }
 }
 
 
 @app.task
 def schedule_task():
+    print("Database access successful")
     conn = psycopg2.connect(user="postgres",
                                   password="root",
                                   database="postgres")
     cur = conn.cursor()
     run_id = str(uuid.uuid4())
     today = datetime.now().date()
-    
+
+    print("In insert query")
     cur.execute("""
         INSERT INTO tasks (run_id, date, status)
         VALUES (%s, %s, %s)
@@ -47,4 +48,5 @@ def schedule_task():
 
 @app.task
 def run_executor():
+    print('in execute task')
     execute_task()
